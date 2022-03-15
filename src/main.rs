fd24995bd1e59f13445c9a893efb7373525da671
OfekShochat <50481015+OfekShochat@users.noreply.github.com>
2022-03-15 15:36:28 +0200

    config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-mod storage;
-mod p2p;
+mod config;
 mod contracts;
+mod p2p;
+mod storage;
 
 fn main() {
     println!("Hello, world!");
+    let config = config::config_from_file("teral.toml");
 }
