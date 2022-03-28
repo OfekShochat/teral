31628c8ab1a94fa360d05ce8d980eaafa1a5d68c
OfekShochat <50481015+OfekShochat@users.noreply.github.com>
2022-03-28 22:11:36 +0300

    error restructure in progress

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,14 @@
-use derive_more::{Display, Error};
 use serde_derive::Deserialize;
+use thiserror::Error;
 
-#[derive(Debug, Deserialize, Error, Display)]
+#[derive(Debug, Deserialize, Error)]
 pub enum Error {
-    SchemaError,
-    GetError,
+    #[error("schema is invalid")]
+    Schema,
+    #[error("a get operation failed")]
+    Get,
+    #[error("an irrecoverable error was occured in the contract executer")]
+    ContractIrrecoverable,
+    #[error("a recoverable error was occured in the contract executer")]
+    ContractRecoverable,
 }
